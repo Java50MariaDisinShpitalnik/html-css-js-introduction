ef532377312b6754544a6cc390e56df45462bd74
Maria Disin Shpitalnik <mariadisin@gmail.com>
2022-12-24 12:29:14 +0200

    Updated Cat's gallery

--- src/catScript.js
+++ src/catScript.js
@@ -2,13 +2,40 @@ const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const catsList = document.querySelector("ul");
 const catsElements = catsList.querySelectorAll("li"); // array of li
+const mainElement = document.querySelector(".main-class");
+const HIDDEN = document.querySelector(".hidden");
+const hideButtonElement = document.getElementById("hide-button");
+const detailsContainer = document.querySelector(".details-container");
+const IS_POINT = ("is-point");
+
+function showDetails() {
+	mainElement.catList.remove(HIDDEN);
+	detailsContainer.classList.add(IS_POINT);
+	setTimeout(function () {
+		detailsContainer.classList.remove(IS_POINT)
+	})
+
+}
+function hideDetails() {
+	mainElement.catList.add(HIDDEN);
+
+	
+}
 
 
 for (let i = 0; i < catsElements.length; ++i) {
 	const catImage = catsElements[i].querySelector(".thumbnails-img");
 	const catTitle = catsElements[i].querySelector(".thumbnails-title");
+	
+	
 	catImage.addEventListener("click", () => {
 		detailsImage.src = catImage.src;
 		detailsTitle.innerHTML = catTitle.innerHTML;
 	});
+	catImage.addEventListener('dblclick', () => {
+		showDetails();
+
+	})
 }
+hideButtonElement.addEventListener("click", hideDetails);
+
